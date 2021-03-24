ec9c2801854212ffdfb2e2c0c7875cba5e44ba74
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-03-23 17:33:46 -0700

    AHHHH

--- FrontEndDeveloper.java
+++ FrontEndDeveloper.java
@@ -102,6 +102,7 @@ private static int numRestaurantsRanked = 100; //default
 
 		// displays the states that have restaurants in the top 100
 		if (localButton == 'l') {
+			System.out.println("AHHHHHHHHHHH");
 			book.getAllStates().distinct().forEach(b -> System.out.println(b));
 		}
 
